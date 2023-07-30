475ae483554b1ddfa9f24e4ec1901d7168db32c2
baka <pathaka895@gmail.com>
2023-07-30 23:21:37 +0530

    recreated the auth script

--- bot/auth.py
+++ bot/auth.py
@@ -1,10 +1,21 @@
 from pyrogram import Client
 
-app = Client(
-    "bot_token",
-    api_id=input("Enter your API ID: "),
-    api_hash=input("Enter your API HASH: "),
-    bot_token=input("Enter Your Bot Token: "))
+
+def create_session():
+    api_id = int(input("Enter your API ID: "))
+    api_hash = input("Enter your API HASH: ")
+    bot_token = input("Enter Your Bot Token: ")
+
+    client = Client(
+        "my_bot_session",
+        api_id=api_id,
+        api_hash=api_hash,
+        bot_token=bot_token
+    )
+
+    return client
+
 
 if __name__ == "__main__":
+    app = create_session()
     app.run()
